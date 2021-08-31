98a6218abf3c698ac6ee8cc6c6f4b23c37161a10
Matias Montaño <matiasmontano@hotmail.com>
2021-08-30 22:43:41 -0300

    formulario de contacto

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index'); // index.js
 //var usersRouter = require('./routes/users');
 var nosotrosRouter = require('./routes/nosotros'); // nosotros.js
--- routes/nosotros.js
+++ routes/nosotros.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('nosotros');
+  res.render('nosotros', {
+    isNosotros: true
+      });
 });
 
 module.exports = router;
